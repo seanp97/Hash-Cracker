cf597d74006afb8cc4d13aeabf293af3bbe22990
seanp97 <sean.pelser97@gmail.com>
2022-04-26 15:38:54 +0100

    Update HashCracker.py

--- HashCracker.py
+++ HashCracker.py
@@ -9,45 +9,17 @@ class Cracker:
 
             self.hash_choice = sys.argv[1]
 
-            self.hash_check = input("Is your password hashed? (y/n): ")
-
             if self.hash_choice.lower() == "md5":
-                if self.hash_check.lower() == "y":
-                    self.Cracker_hashed_md5()
-                elif self.hash_check.lower() == "n":
-                    self.Cracker_unhashed_md5()
+                self.Cracker_hashed_md5()
 
             elif self.hash_choice.lower() == "sha1":
-                if self.hash_check.lower() == "y":
-                    self.Cracker_hashed_sha1()
-                elif self.hash_check.lower() == "n":
-                    self.Cracker_unhashed_sha1()
+                self.Cracker_hashed_sha1()
 
             elif self.hash_choice.lower() == "sha256":
-                if self.hash_check.lower() == "y":
-                    self.Cracker_hashed_sha256()
-                elif self.hash_check.lower() == "n":
-                    self.Cracker_unhashed_sha256()
+                self.Cracker_hashed_sha256()
+                
         else:
-
             print("Choose a hashing method")
-
-
-    def Cracker_unhashed_md5(self):
-        self.password_unhashed_md5 = input("Enter unhashed password: ")
-
-        self.md5file_unhashed_md5 = open("rockyou.txt", "r", encoding="ISO-8859-1")
-
-        self.exists_unhashed_md5 = False
-
-        for x_unhashed_md5 in self.md5file_unhashed_md5:
-            if x_unhashed_md5.strip() == self.password_unhashed_md5.strip():
-                print("Match: " + hashlib.md5(x_unhashed_md5.strip().encode('utf-8')).hexdigest())
-                self.exists_unhashed_md5 = True
-                return
-
-        if self.exists_unhashed_md5 == False:
-            print("No match")
 
 
 
@@ -69,23 +41,6 @@ class Cracker:
 
 
 
-    def Cracker_unhashed_sha1(self):
-        self.password_unhashed_sha1 = input("Enter unhashed password: ")
-
-        self.sha1file_unhashed_sha1 = open("rockyou.txt", "r", encoding="ISO-8859-1")
-
-        self.exists_unhashed_sha1 = False
-
-        for x_unhashed_sha1 in self.sha1file_unhashed_sha1:
-            if x_unhashed_sha1.strip() == self.password_unhashed_sha1.strip():
-                print("Match: " + hashlib.sha1(x_unhashed_sha1.strip().encode('utf-8')).hexdigest())
-                self.exists_unhashed_sha1 = True
-                return
-
-        if self.exists_unhashed_sha1 == False:
-            print("No match")
-
-
     def Cracker_hashed_sha1(self):
         self.password_hashed_sha1 = input("Enter hashed password: ")
 
@@ -103,22 +58,6 @@ class Cracker:
         if self.exists_hashed_sha1 == False:
             print("No match")
 
-
-    def Cracker_unhashed_sha256(self):
-        self.password_unhashed_sha256 = input("Enter unhashed password: ")
-
-        self.sha256file_unhashed_sha256 = open("rockyou.txt", "r", encoding="ISO-8859-1")
-
-        self.exists_unhashed_sha256 = False
-
-        for x_unhashed_sha256 in self.sha256file_unhashed_sha256:
-            if x_unhashed_sha256.strip() == self.password_unhashed_sha256.strip():
-                print("Match: " + hashlib.sha256(x_unhashed_sha256.strip().encode('utf-8')).hexdigest())
-                self.exists_unhashed_sha256 = True
-                return
-
-        if self.exists_unhashed_sha256 == False:
-            print("No match")
 
 
     def Cracker_hashed_sha256(self):
