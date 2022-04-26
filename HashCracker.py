9fcddd1899ff5a42c759aeccd840d9934c5e5397
seanp97 <sean.pelser97@gmail.com>
2022-04-26 15:42:32 +0100

    Update HashCracker.py

--- HashCracker.py
+++ HashCracker.py
@@ -9,6 +9,8 @@ class Cracker:
 
             self.hash_choice = sys.argv[1]
 
+            self.hashed_password = sys.argv[2]
+
             if self.hash_choice.lower() == "md5":
                 self.Cracker_hashed_md5()
 
@@ -17,22 +19,20 @@ class Cracker:
 
             elif self.hash_choice.lower() == "sha256":
                 self.Cracker_hashed_sha256()
-                
+
         else:
             print("Choose a hashing method")
 
 
 
     def Cracker_hashed_md5(self):
-        self.password_hashed_md5 = input("Enter hashed password: ")
-
         self.md5file_hashed_md5 = open("rockyou.txt", "r", encoding="ISO-8859-1")
 
         self.exists_hashed_md5 = False
 
         for x_hashed_md5 in self.md5file_hashed_md5:
-            if hashlib.md5(x_hashed_md5.strip().encode('utf-8')).hexdigest() == self.password_hashed_md5.strip():
-                print(f"Match {x_hashed_md5.strip()} :  {self.password_hashed_md5}")
+            if hashlib.md5(x_hashed_md5.strip().encode('utf-8')).hexdigest() == self.hashed_password.strip():
+                print(f"Match {x_hashed_md5.strip()} :  {self.hashed_password}")
                 self.exists_hashed_md5 = True
                 return
 
@@ -42,16 +42,14 @@ class Cracker:
 
 
     def Cracker_hashed_sha1(self):
-        self.password_hashed_sha1 = input("Enter hashed password: ")
-
         self.sha1file_hashed_sha1 = open("rockyou.txt", "r", encoding="ISO-8859-1")
 
         self.exists_hashed_sha1 = False
 
 
         for x_hashed_sha1 in self.sha1file_hashed_sha1:
-            if hashlib.sha1(x_hashed_sha1.strip().encode('utf-8')).hexdigest() == self.password_hashed_sha1.strip():
-                print(f"Match {x_hashed_sha1.strip()} :  {self.password_hashed_sha1}")
+            if hashlib.sha1(x_hashed_sha1.strip().encode('utf-8')).hexdigest() == self.hashed_password.strip():
+                print(f"Match {x_hashed_sha1.strip()} :  {self.hashed_password}")
                 self.exists_hashed_sha1 = True
                 return
 
@@ -61,16 +59,13 @@ class Cracker:
 
 
     def Cracker_hashed_sha256(self):
-        self.password_hashed_sha256 = input("Enter hashed password: ")
-
         self.sha256file_hashed_sha256 = open("rockyou.txt", "r", encoding="ISO-8859-1")
 
         self.exists_hashed_sha256 = False
 
-
         for x_hashed_sha256 in self.sha256file_hashed_sha256:
-            if hashlib.sha256(x_hashed_sha256.strip().encode('utf-8')).hexdigest() == self.password_hashed_sha256.strip():
-                print(f"Match {x_hashed_sha256.strip()} :  {self.password_hashed_sha256}")
+            if hashlib.sha256(x_hashed_sha256.strip().encode('utf-8')).hexdigest() == self.hashed_password.strip():
+                print(f"Match {x_hashed_sha256.strip()} :  {self.hashed_password}")
                 self.exists_hashed_sha256 = True
                 return
 
